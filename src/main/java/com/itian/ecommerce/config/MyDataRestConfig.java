a5af24f17c0ca8e48192b2561752a8c994633cac
Ibrahim Mohsen <i.Mohsen-c@tamkeentech.sa>
2020-06-14 22:20:13 +0300

    Product and Product Categories for e-commerce service

---
+++ src/main/java/com/itian/ecommerce/config/MyDataRestConfig.java
@@ -0,0 +1,56 @@
+package com.itian.ecommerce.config;
+
+import com.itian.ecommerce.entity.Product;
+import com.itian.ecommerce.entity.ProductCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    private EntityManager entityManager;
+
+    @Autowired
+    public MyDataRestConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST ,HttpMethod.DELETE};
+
+        //Disable HTTP Methods for Product: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+
+        //Disable HTTP Methods for ProductRepository: PUT, POST and DELETE
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+        // spring data rest by default not return th entity id so we need to configure abd expose it
+        exposeIds(config);
+    }
+
+    private void exposeIds(RepositoryRestConfiguration config) {
+
+        Set<EntityType<?>> entities =entityManager.getMetamodel().getEntities();
+        List<Class> entityClasses = new ArrayList<>();
+        for (EntityType entityType : entities) {
+            entityClasses.add(entityType.getJavaType());
+        }
+        Class[] domainTypes = entityClasses.toArray(new Class[0]);
+        config.exposeIdsFor(domainTypes);
+    }
+}
---
+++ src/main/java/com/itian/ecommerce/entity/Product.java
@@ -0,0 +1,47 @@
+package com.itian.ecommerce.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private ProductCategory category;
+
+    private String sku;
+
+    private String name;
+
+    private String description;
+
+    @Column(name = "unit_price")
+    private BigDecimal unitPrice;
+
+    private String imageUrl;
+
+    private Boolean active;
+
+    @Column(name = "units_in_stock")
+    private Integer unitsInStock;
+
+    @Column(name = "date_created")
+    @CreationTimestamp
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    @UpdateTimestamp
+    private Date lastUpdated;
+}
